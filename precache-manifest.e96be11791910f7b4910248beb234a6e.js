1f8e71f0331cabf09c6b376238784a631d90c271
Zane Russell <zanerussell@zanes-mbp.lan>
2019-03-03 14:37:37 -0700

    Updates

---
+++ precache-manifest.e96be11791910f7b4910248beb234a6e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f03fe97195c85196a98015d615d63cb",
+    "url": "/kate-dinsmore-updated/static/media/twitter.0f03fe97.svg"
+  },
+  {
+    "revision": "3b55286d6fc6f3886de9",
+    "url": "/kate-dinsmore-updated/static/css/main.c29c62e9.chunk.css"
+  },
+  {
+    "revision": "f4248386c7a534fd3af9",
+    "url": "/kate-dinsmore-updated/static/js/runtime~main.f4248386.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/kate-dinsmore-updated/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "373bbea13e1d9697b9104fe7c1e5323f",
+    "url": "/kate-dinsmore-updated/static/media/kieldrum.373bbea1.jpg"
+  },
+  {
+    "revision": "cc02fd64033fc774d0458f98e00d0d0e",
+    "url": "/kate-dinsmore-updated/static/media/albuminsert.cc02fd64.jpg"
+  },
+  {
+    "revision": "aa19a890ed532e96ae14a9f43c47fd5f",
+    "url": "/kate-dinsmore-updated/static/media/facebook.aa19a890.svg"
+  },
+  {
+    "revision": "3b55286d6fc6f3886de9",
+    "url": "/kate-dinsmore-updated/static/js/main.3b55286d.chunk.js"
+  },
+  {
+    "revision": "39a26959b8c252f24f9af31d2e05c1db",
+    "url": "/kate-dinsmore-updated/static/media/instagram.39a26959.svg"
+  },
+  {
+    "revision": "d3c6fa229c3fc946312183a4ba7b1c07",
+    "url": "/kate-dinsmore-updated/static/media/albumrelease.d3c6fa22.jpg"
+  },
+  {
+    "revision": "dedc01909ab78ddbfc76ef108a668fa8",
+    "url": "/kate-dinsmore-updated/static/media/facebookwhite.dedc0190.svg"
+  },
+  {
+    "revision": "447540e107267c8406e68fcd09c780ec",
+    "url": "/kate-dinsmore-updated/static/media/twitterwhite.447540e1.svg"
+  },
+  {
+    "revision": "1c0000aa2364d2d547ff8c664be72488",
+    "url": "/kate-dinsmore-updated/static/media/instagramwhite.1c0000aa.svg"
+  },
+  {
+    "revision": "8cc28ac24bac850f42b2766186dd8eda",
+    "url": "/kate-dinsmore-updated/static/media/mailwhite.8cc28ac2.svg"
+  },
+  {
+    "revision": "b1325dd906b0a7d15984f3a6e21943b5",
+    "url": "/kate-dinsmore-updated/index.html"
+  }
+];
